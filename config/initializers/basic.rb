93a09db3eb86b3a3536ac121e9adb40e9dd16e86
xdlong <xiongdl2009@163.com>
2015-06-01 20:28:18 +0800

    udpate

--- config/initializers/basic.rb
+++ config/initializers/basic.rb
@@ -1,3 +1,3 @@
 require 'basic'
-REDIS = Redis.new({host:xiongdl2009.vicp.cc,port:11073})
+# REDIS = Redis.new({host:xiongdl2009.vicp.cc,port:13691})
 # Time.class_eval("def self.demongoize(object);(ret=super(object)) ? ret+(8*60*60) : ret;end")
